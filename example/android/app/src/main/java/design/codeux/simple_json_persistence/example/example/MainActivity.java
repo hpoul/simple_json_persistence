cc1af91bc284b5c763a38da1072847ff3d25f713
Herbert Poul <herbert@poul.at>
2019-08-09 11:43:04 +0200

    flutter example application scaffolding

---
+++ example/android/app/src/main/java/design/codeux/simple_json_persistence/example/example/MainActivity.java
@@ -0,0 +1,13 @@
+package design.codeux.simple_json_persistence.example.example;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
